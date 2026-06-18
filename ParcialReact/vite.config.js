781644f782887489f2adb05967c9d83869721c5e
ezequiel developer <gorositoeze703@gmail.com>
2026-06-17 23:38:35 -0300

    modificaciones para pages

--- ParcialReact/vite.config.js
+++ ParcialReact/vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
 
-
+// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   base: '/ParcialReact/',
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
 })
